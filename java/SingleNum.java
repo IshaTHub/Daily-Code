9ea2c50c61ed07b6ecef8b0b9479c207014c3174
Isha Tiwari <85096427+IshaT-Hub@users.noreply.github.com>
2024-12-18 23:16:11 +0530

    SingleNum.java
    Ques: 136. Single Number (easy)
    https://leetcode.com/problems/single-number/description/

---
+++ java/SingleNum.java
@@ -0,0 +1,17 @@
+// Ques: 136. Single Number (easy)
+// https://leetcode.com/problems/single-number/description/
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        if(len == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        for(int i = 1; i < len - 1; i++){
+           if(nums[i] != nums[i - 1] && nums[i] != nums[i + 1])
+               return nums[i];
+        }
+        return nums[len - 1];
+    }
+}
